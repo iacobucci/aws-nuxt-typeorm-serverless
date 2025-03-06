b3fb2def0bae7347f6f2466064f61e6a2f2c7feb
iacobucci <iacobuccivalerio@gmail.com>
2025-03-06 19:27:29 +0100

    modified:   cloudformation/serverless.yml 	modified:   pages/index.vue 	new file:   server/api/getinfo.ts

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+const env = ref(null)
+const hostname = ref(null)
+const port = ref(null)
+const name = ref(null)
+const username = ref(null)
+const password = ref(null)
+
+onMounted(async () => {
+	const response = await $fetch('/api/getinfo') as any
+	env.value = response.env
+	hostname.value = response.hostname
+	port.value = response.port
+	name.value = response.name
+	username.value = response.username
+	password.value = response.password
+})
+
+</script>
 
 <template>
 	<div>
+
+		<div> NODE_ENV: {{ env }}</div>
+		<div> HOSTNAME: {{ hostname }}</div>
+		<div> PORT: {{ port }}</div>
+		<div> NAME: {{ name }}</div>
+		<div> USERNAME: {{ username }}</div>
+		<div> PASSWORD: {{ password }}</div>
+
 		Page: index
 		<Testcomponent></Testcomponent>
 		<Testparent></Testparent>
---
+++ server/api/getinfo.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler((event) => {
+	let env = process.env.NODE_ENV
+	let hostname = process.env.DB_HOSTNAME
+	let port = process.env.DB_PORT
+	let name = process.env.DB_NAME
+	let username = process.env.DB_USERNAME
+	let password = process.env.DB_PASSWORD
+
+	return {
+		env, hostname, port, name, username, password
+	}
+})
